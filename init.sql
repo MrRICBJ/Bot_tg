c999c324298e08d836350eb02678e512ac25bcfa
Bjanette <92569206+MrRICBJ@users.noreply.github.com>
2023-03-21 13:24:37 +0300

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE movies (
-    id BIGINT PRIMARY KEY,
-    user_id integer NOT NULL,
-    movie_id integer NOT NULL,
-    genre character varying(255) NOT NULL,
-    movie_name character varying(255) NOT NULL,
-    "time" timestamp without time zone NOT NULL
-);
